1615c4e2111056c1e70ba81f9d629366863ef7ce
Mike5719 <149823846+Mike5719@users.noreply.github.com>
2024-02-03 00:38:38 -0500

    header, footer, navigation, routing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
-//TODO: FINISH
+import React from 'react';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 export default function Footer() {
     return(
 <footer className="profile-footer">
-        <a href= "https://github.com" target= '_blank' label='Github'></a>     
-        <a href= "https://linkedin.com" target= '_blank' label='LinkedIn'></a>   
-        <a href= "https://twitter.com" target= '_blank' label='Twitter'></a>      
+        <a href= "https://github.com" target= '_blank'> <FaGithub/> </a>    
+        <a href= "https://linkedin.com" target= '_blank'> <FaLinkedin/> </a>   
+        <a href= "https://twitter.com" target= '_blank'> <FaTwitter/> </a>      
       </footer>
     );
-  
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,10 @@
-export default function Nav({ links }) {
+import NavTabs from './Navigation';
+
+export default function Header() {
     return (
-      <nav className="navbar navbar-expand-lg bg-secondary">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {links.map((link) => link)}
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <header>
+        <NavTabs/>
+    </header>
     );
-  }
+  };
   
