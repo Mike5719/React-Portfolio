dd451a43337bb171d02cd92fb2a1b0040aa40d8c
Mike5719 <149823846+Mike5719@users.noreply.github.com>
2024-03-10 16:28:00 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer className="footer-icons">
       <a href="https://github.com/Mike5719" target='_blank'> <FaGithub /> </a>
       <a href="https://linkedin.com" target='_blank'> <FaLinkedin /> </a>
-      <a href="https://twitter.com" target='_blank'> <FaTwitter /> </a>
     </footer>
   );
 };
